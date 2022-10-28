c88015b1c71ac7ff62ab55aa2a5aad57217a9e13
FranciscoRomeroGuill <FranciscoRomeroGuill@CSESTPC12002-CD>
2022-10-28 08:29:30 +0200

    Esquema inicial de una aplicación FXML

---
+++ src/main/java/com/mycompany/hellofxml/PrimaryController.java
@@ -0,0 +1,23 @@
+package com.mycompany.hellofxml;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class PrimaryController {
+
+    @FXML
+    private Button primaryButton;
+    @FXML
+    private Label texto;
+    @FXML
+    private TextField cajetin;
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        //App.setRoot("secondary");
+        texto.setText("Hola a " + cajetin.getText());
+    }
+}
